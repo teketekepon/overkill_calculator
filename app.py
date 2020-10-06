9333839d7f8764b5fddbacb2f31a3a7979c87ef4
Teketeke <teketeke.0408@gmail.com>
2020-10-07 01:42:19 +0900

    make it better

--- app.py
+++ app.py
@@ -15,11 +15,25 @@ class App(ttk.Frame):
         self.bosshp = tk.StringVar()
         self.damege = tk.StringVar()
         self.st = tk.BooleanVar(False)
+        self.tgl = False
         # Criate Widgets
         self.create_widgets()
         self.hp_entry.focus_set()
+        self.softkey = ttk.Frame(borderwidth=5, width=260,
+                                 height=100, relief='groove')
+        self.create_softkey()
+        # Bind Key events
         self.master.bind_all("<Return>", lambda *o: self.attack())
         self.master.bind_all("<equal>", lambda *o: self.attack())
+        self.master.bind_all("<Delete>", lambda *o: self.reset())
+        self.master.bind_all("<period>", lambda *o: self.reset())
+        self.master.bind_all("<Escape>", lambda *o: self.reset())
+        self.master.bind_all("<Button-1>", self.click_entry)
+        self.master.bind_all("<Tab>", self.state_key)
+        self.master.bind_all("<slash>", self.state_key)
+        self.master.bind_all("<asterisk>", self.state_key)
+        self.master.bind_all("<minus>", self.state_key)
+        self.master.bind_all("<plus>", self.state_key)
 
     def attack(self):
         try:
@@ -38,7 +52,6 @@ class App(ttk.Frame):
                     t = '持ち越し時間は90秒です'
                 else:
                     t = f'持ち越し時間は{r}秒です'
-
         self.result.configure(state='normal')
         self.result.delete('1.0', 'end')
         self.result.insert('1.0', t)
@@ -51,34 +64,26 @@ class App(ttk.Frame):
         self.result.configure(state='disabled')
         self.result.grid(row=0, column=0, columnspan=2, pady=10)
         # Label
-        ttk.Label(self, text=u'ボス残り体力').grid(column=0, row=1, padx=5)
-        ttk.Label(self, text=u'推定ダメージ').grid(column=0, row=2, padx=5)
+        ttk.Label(self, text=u'ボス残り体力', font=('游ゴシック', 13)
+                  ).grid(column=0, row=1, padx=5)
+        ttk.Label(self, text=u'推定ダメージ', font=('游ゴシック', 13)
+                  ).grid(column=0, row=2, padx=5)
         # Entry
         self.hp_entry = ttk.Entry(self, textvariable=self.bosshp, width=10)
         self.hp_entry.grid(column=1, row=1, padx=10, pady=5)
         self.dmg_entry = ttk.Entry(self, textvariable=self.damege, width=10)
         self.dmg_entry.grid(column=1, row=2, padx=10, pady=5)
-
-
-class Softkey(ttk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        ttk.Button(master, text='ソフトキー切り替え',
+        ttk.Button(self, text='ソフトキー切り替え',
                    command=self.toggle).grid(column=0, row=3, columnspan=2)
-        self.a = master
-        self.softkey = ttk.Frame(borderwidth=5, width=260,
-                                 height=100, relief='groove')
-        self.tgl = False
-        self.create_widgets()
-        # Binding Keys
-        master.bind_all("<Button-1>", self.click_entry)
-        master.bind_all("<Delete>", lambda *o: self.reset())
-        master.bind_all("<period>", lambda *o: self.reset())
-        master.bind_all("<Tab>", self.state_key)
-        master.bind_all("<slash>", self.state_key)
-        master.bind_all("<asterisk>", self.state_key)
-        master.bind_all("<minus>", self.state_key)
-        master.bind_all("<plus>", self.state_key)
+
+    def click_entry(self, event):
+        focus = self.focus_get()
+        if focus == self.hp_entry:
+            self.st.set(False)
+            self.sbutton['text'] = 'ボス残り体力'
+        elif focus == self.dmg_entry:
+            self.st.set(True)
+            self.sbutton['text'] = '推定ダメージ'
 
     def toggle(self):
         self.tgl = not self.tgl
@@ -88,89 +93,76 @@ class Softkey(ttk.Frame):
             self.softkey.pack_forget()
 
     def ins(self, n):
-        st = self.a.st.get()
-        if st:
+        if self.st.get():
             try:
-                m = int(self.a.damege.get())
-                self.a.damege.set(str(m*10+n))
+                m = int(self.damege.get())
+                self.damege.set(str(m*10+n))
             except Exception:
-                self.a.damege.set(str(n))
+                self.damege.set(str(n))
         else:
             try:
-                m = int(self.a.bosshp.get())
-                self.a.bosshp.set(str(m*10+n))
+                m = int(self.bosshp.get())
+                self.bosshp.set(str(m*10+n))
             except Exception:
-                self.a.bosshp.set(str(n))
+                self.bosshp.set(str(n))
 
     def tens(self):
-        st = self.a.st.get()
         try:
-            if st:
-                m = int(self.a.damege.get())
-                self.a.damege.set(str(m*10000))
-                # self.a.damege.set(self.a.damege.get() + '0000')
+            if self.st.get():
+                m = int(self.damege.get())
+                self.damege.set(str(m*10000))
+                # self.damege.set(self.damege.get() + '0000')
             else:
-                m = int(self.a.bosshp.get())
-                self.a.bosshp.set(str(m*10000))
+                m = int(self.bosshp.get())
+                self.bosshp.set(str(m*10000))
         except Exception:
             pass
 
     def back(self):
-        st = self.a.st.get()
         try:
-            if st:
-                m = self.a.damege.get()
-                self.a.damege.set(m[0:-1])
+            if self.st.get():
+                m = self.damege.get()
+                self.damege.set(m[0:-1])
             else:
-                m = self.a.bosshp.get()
-                self.a.bosshp.set(m[0:-1])
+                m = self.bosshp.get()
+                self.bosshp.set(m[0:-1])
         except Exception:
             pass
 
     def reset(self):
-        self.a.bosshp.set('')
-        self.a.damege.set('')
-        self.a.result.configure(state='normal')
-        self.a.result.delete('1.0', 'end')
-        self.a.result.configure(state='disabled')
-        return
+        self.bosshp.set('')
+        self.damege.set('')
+        self.result.configure(state='normal')
+        self.result.delete('1.0', 'end')
+        self.result.configure(state='disabled')
 
     def state_button(self):
-        st = self.a.st.get()
-        if st:
-            self.a.hp_entry.focus_set()
+        st = self.st.get()
+        if self.st.get():
+            self.hp_entry.focus_set()
             self.sbutton['text'] = 'ボス残り体力'
         else:
-            self.a.dmg_entry.focus_set()
+            self.dmg_entry.focus_set()
             self.sbutton['text'] = '推定ダメージ'
-        self.a.st.set(not st)
-
-    def click_entry(self, event):
-        focus = self.focus_get()
-        if focus == self.a.hp_entry:
-            self.a.st.set(False)
-            self.sbutton['text'] = 'ボス残り体力'
-        if focus == self.a.dmg_entry:
-            self.a.st.set(True)
-            self.sbutton['text'] = '推定ダメージ'
+        self.st.set(not st)
 
     def state_key(self, event):
-        st = self.a.st.get()
+        st = self.st.get()
         if not event.keysym == 'Tab':
             if st:
-                self.a.damege.set(self.a.damege.get().rstrip('+-*/'))
+                self.damege.set(self.damege.get().rstrip('+-*/'))
             else:
-                self.a.bosshp.set(self.a.bosshp.get().rstrip('+-*/'))
+                self.bosshp.set(self.bosshp.get().rstrip('+-*/'))
         if st:
-            self.a.hp_entry.focus_set()
+            self.hp_entry.focus_set()
             self.sbutton['text'] = 'ボス残り体力'
         else:
-            self.a.dmg_entry.focus_set()
+            self.dmg_entry.focus_set()
             self.sbutton['text'] = '推定ダメージ'
-        self.a.st.set(not st)
+        self.st.set(not st)
 
-    def create_widgets(self):
-        # tenkey1-9
+    def create_softkey(self):
+        # Tenkey1-9
         for i in range(1, 10):
             column = (i+2) % 3
             row = -((i-1)//3)+2
@@ -182,25 +174,22 @@ class Softkey(ttk.Frame):
         # 0
         ttk.Button(self.softkey, text='0',
                    command=lambda: self.ins(0)).grid(column=1, row=3)
-        # backspace
+        # Backspace
         ttk.Button(self.softkey, text='Backspace',
                    command=self.back).grid(column=2, row=3)
-        # reset
+        # Reset
         ttk.Button(self.softkey, text='Reset',
                    command=self.reset).grid(column=0, row=4)
-        # state
+        # State
         self.sbutton = ttk.Button(self.softkey, text='ボス残り体力',
                                   command=self.state_button)
         self.sbutton.grid(column=1, row=4)
-        # attack
+        # Attack
         ttk.Button(self.softkey, text='Attack',
-                   command=lambda: App.attack(self.a)).grid(column=2, row=4)
+                   command=self.attack).grid(column=2, row=4)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    style = ttk.Style()
-    style.configure('TLabel', font=('游ゴシック', 13))
     app = App(master=root)
-    Softkey(master=app)
-    root.mainloop()
+    app.mainloop()
